42d8c8839eed27a380cf6b073aefc1f063ecb637
Shoichi Kaji <skaji@cpan.org>
2014-05-20 23:00:00 +0900

    add Tasks

---
+++ lib/Dist/Template/Tasks.pm
@@ -0,0 +1,97 @@
+package Dist::Template::Tasks;
+use strict;
+use warnings;
+use utf8;
+
+use CPAN::Meta;
+use Config;
+use Daiku;
+use Module::CPANfile;
+use Pod::Markdown;
+
+sub slurp {
+    open my $fh, "<:utf8", $_[0] or die "open $_[0]: $!\n";
+    local $/; <$fh>
+}
+sub spew  {
+    open my $fh, ">:utf8", $_[0] or die "open $_[0]: $!\n";
+    print {$fh} $_[1]
+}
+sub module_path {
+    my $module = shift;
+    $module =~ s{::}{/}g;
+    "lib/$module.pm";
+}
+sub dist_name {
+    my $module = shift;
+    $module =~ s{::}{-}g;
+    $module;
+}
+
+
+my $MAKE = $Config{make};
+sub import {
+    (undef, my $module) = @_;
+    my $module_path = module_path($module);
+    my $dist_name   = dist_name($module);
+
+    my $caller = caller;
+
+    if (defined \&{ $caller . "::engine" }) {
+        no warnings 'redefine';
+        *engine = \&{ $caller . "::engine" };
+    }
+
+    task default => 'all';
+
+    desc '(this is default) test, regen';
+    task all => [qw(test regen)];
+
+    desc 'build only';
+    task build => 'Makefile' => sub {
+        sh $MAKE;
+    };
+
+    desc "run test cases";
+    task test => 'Makefile' => sub {
+        sh $MAKE, 'test';
+    };
+
+    desc "cleanup";
+    task clean => 'Makefile' => sub {
+        sh $MAKE, 'realclean';
+    };
+
+    desc "regenerate README.md and META.json";
+    task regen => ['README.md', 'META.json'];
+
+    file 'Makefile' => 'Makefile.PL' => sub {
+        sh $^X, "Makefile.PL";
+    };
+
+    file 'README.md' => $module_path => sub {
+        my $p = Pod::Markdown->new;
+        $p->output_string(\my $markdown);
+        $p->parse_string_document(slurp $module_path);
+        spew "README.md", $markdown;
+    };
+
+
+    file 'META.json' => ['Makefile', 'cpanfile', $module_path] => sub {
+        sh $MAKE, "metafile";
+        my ($dir) = grep -d, glob "$dist_name-*";
+
+        my $meta = CPAN::Meta->load_file("$dir/META.json");
+        my $prereqs = Module::CPANfile->load("cpanfile")
+            ->prereqs->as_string_hash;
+
+        CPAN::Meta->new({
+            %{$meta->as_struct},
+            dynamic_config => 0,
+            prereqs => $prereqs
+        })->save("META.json");
+
+    };
+}
+
+1;
